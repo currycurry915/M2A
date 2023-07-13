282674a383d111c9c070d7c33d374c6a74d76f7d
currycurry915 <99167410+currycurry915@users.noreply.github.com>
2023-07-13 14:14:14 +0900

    Add files via upload

--- tuneavideo/util.py
+++ tuneavideo/util.py
@@ -12,7 +12,21 @@ from tqdm import tqdm
 from einops import rearrange
 
 
-def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=4, fps=8):
+# def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=4, fps=8):
+#     videos = rearrange(videos, "b c t h w -> t b c h w")
+#     outputs = []
+#     for x in videos:
+#         x = torchvision.utils.make_grid(x, nrow=n_rows)
+#         x = x.transpose(0, 1).transpose(1, 2).squeeze(-1)
+#         if rescale:
+#             x = (x + 1.0) / 2.0  # -1,1 -> 0,1
+#         x = (x * 255).numpy().astype(np.uint8)
+#         outputs.append(x)
+
+#     os.makedirs(os.path.dirname(path), exist_ok=True)
+#     imageio.mimsave(path, outputs, fps=fps)
+
+def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=4, duration=3.2):
     videos = rearrange(videos, "b c t h w -> t b c h w")
     outputs = []
     for x in videos:
@@ -24,7 +38,7 @@ def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=4, f
         outputs.append(x)
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    imageio.mimsave(path, outputs, fps=fps)
+    imageio.mimsave(path, outputs, duration=duration)
 
 
 # DDIM Inversion
